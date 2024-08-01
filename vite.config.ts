7252e4e9b1b9e0c4f9312d6547d7984f8fcfad77
Vladimir <58871317+chop5551@users.noreply.github.com>
2024-08-01 12:51:16 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Youtube-Shorts-Clone/', // Убедитесь, что путь соответствует вашему репозиторию
+  plugins: [react()]
+});
